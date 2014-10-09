5a182624d6e3e6af9087b3773ede18451c17390f
PaolaMontes <paolamontest@misena.edu.co>
2014-10-09 21:45:52 +0000

    cambios

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
-<<<<<<< HEAD
+
 Rails.application.config.session_store :cookie_store, key: '_prueba41_session'
-=======
-Rails.application.config.session_store :cookie_store, key: '_prueba_session'
->>>>>>> dd22eab6b723b17872c6e3f92f5d5923fe858b3b
+
